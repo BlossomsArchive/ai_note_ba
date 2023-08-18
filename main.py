19094cbf78524685f835a504ba284798857da7ad
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-08-19 01:16:42 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import markovify
+import os
+from misskey import Misskey
 
 input = open('./model.txt', 'r', encoding='utf-8')
 model = markovify.NewlineText(input.read())
@@ -6,5 +8,13 @@ sentence = model.make_sentence()
 
 note_text = sentence.replace(" ","")
 print(note_text)
+
+#SNS投稿API
+# Misskey
+misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
+misskey_token = os.environ.get("MISSKEY_TOKEN")
+api = Misskey(misskey_address)
+api.token = misskey_token
+api.notes_create(text=note_text)
 
 input.close()
