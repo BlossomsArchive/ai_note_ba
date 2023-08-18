d948e497dd5a447a8cf8e1d5cb8467354fd5a2b0
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-08-19 01:42:55 +0900

    Update model-make.py

--- model/model-make.py
+++ model/model-make.py
@@ -1,7 +1,7 @@
 import MeCab
 
-input = open('/model/base.txt', 'r', encoding='utf-8') 
-output = open('/model.txt', 'w', encoding='utf-8') 
+input = open('model/base.txt', 'r', encoding='utf-8') 
+output = open('model.txt', 'w', encoding='utf-8') 
 
 mecab = MeCab.Tagger("-Owakati")
 
