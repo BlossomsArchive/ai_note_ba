46a991c227aa5584a2bf7502ecd89f16d7cfc17b
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-09-25 15:50:44 +0900

    main.py の更新

--- main.py
+++ main.py
@@ -13,14 +13,20 @@ print(note_text)
 #SNS投稿API
 # Misskey
 misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
+print(1)
 misskey_token = os.environ.get("MISSKEY_TOKEN")
+print(2)
 api = Misskey(misskey_address)
+print(3)
 api.token = misskey_token
+print(4)
 input.close()
+print(5)
 
 while True:
     try:
         api.notes_create(text=note_text)
+print(6)
     except:
         time.sleep(300)
     else:
