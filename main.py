17a5c302a679f81692e486f32cd536ef647b0355
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-09-25 10:39:47 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import markovify
 import os
 from misskey import Misskey
+import time
 
 input = open('./model.txt', 'r', encoding='utf-8')
 model = markovify.NewlineText(input.read())
@@ -15,6 +16,12 @@ misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
 misskey_token = os.environ.get("MISSKEY_TOKEN")
 api = Misskey(misskey_address)
 api.token = misskey_token
-api.notes_create(text=note_text)
-
 input.close()
+
+while True:
+    try:
+        api.notes_create(text=note_text)
+    except:
+        time.sleep(300)
+    else:
+        break
