17c99e6de5e2323ba0f5f977396053246fe81184
MizuiMiduki <105696149+MizuiMiduki@users.noreply.github.com>
2023-09-28 21:52:24 +0900

    update base

--- model/model-make.py
+++ model/model-make.py
@@ -1,14 +1,19 @@
 import MeCab
+import os
 
-input = open('model/base.txt', 'r', encoding='utf-8') 
-output = open('model.txt', 'w', encoding='utf-8') 
+dir = "model/"
+file_count = sum(os.path.isfile(os.path.join(dir,name)) for name in os.listdir(dir))
 
-mecab = MeCab.Tagger("-Owakati")
+for i in range (1,file_count):
+	input = open('model/base_'+i+'.txt', 'r', encoding='utf-8') 
+	output = open('model_'+i+'.txt', 'w', encoding='utf-8') 
 
-for line in input.read().split('\n'):
-	splittedLine = ' '.join(mecab.parse(line).split())
-	output.write(splittedLine)
-	output.write('\n')
+	mecab = MeCab.Tagger("-Owakati")
 
-input.close()
-output.close()
+	for line in input.read().split('\n'):
+		splittedLine = ' '.join(mecab.parse(line).split())
+		output.write(splittedLine)
+		output.write('\n')
+
+	input.close()
+	output.close()
