1c0dd8903afa70b78746b591c90241e5d010c738
瑞井 箕月 <6161.k.iga@gmail.com>
2024-08-26 22:45:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 
 input = open('./model.txt', 'r', encoding='utf-8')
 model = markovify.NewlineText(input.read())
-sentence = model.make_sentence()
+sentence = model.make_short_sentence()
 
 note_text = sentence.replace(" ","")
 print(note_text)
