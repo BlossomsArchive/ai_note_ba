e7a3bd54dce17f7ce1f647e5415f71c30a2d9ffb
瑞井 箕月 <6161.k.iga@gmail.com>
2024-08-26 23:06:12 +0900

    Add files via upload

--- model/model-make.py
+++ model/model-make.py
@@ -1,19 +1,23 @@
 import MeCab
+import gzip
 import os
 
 dir = "model/"
 file_count = sum(os.path.isfile(os.path.join(dir,name)) for name in os.listdir(dir))
 
 for i in range (1,file_count):
-	input = open('model/base_{}.txt'.format(i), 'r', encoding='utf-8') 
-	output = open('model_{}.txt'.format(i), 'w', encoding='utf-8') 
+	input = open('model/base_{}.txt'.format(i), 'r', encoding='utf-8')
 
 	mecab = MeCab.Tagger("-Owakati")
 
+	output=""
+
 	for line in input.read().split('\n'):
 		splittedLine = ' '.join(mecab.parse(line).split())
-		output.write(splittedLine)
-		output.write('\n')
+		output += splittedLine
+		output += '\n'
 
-	input.close()
-	output.close()
+input.close()
+
+with gzip.open("/model.gz'", "wt", encoding="utf-8") as f:
+	f.write(output)
